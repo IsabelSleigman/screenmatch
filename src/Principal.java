6361a103767cf26e3072411ad1882df47d60d1b2
isabel.sleigman <sleimusic@gmail.com>
2023-06-22 16:00:53 -0300

    Criado Classe generica para trabalhar com herança, acrescentado classe serie, ajustado classe filme

---
+++ src/Principal.java
@@ -0,0 +1,17 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+
+        meuFilme.exibeFichaTecnica();
+
+        meuFilme.avalia(8);
+        meuFilme.avalia(5);
+        meuFilme.avalia(10);
+
+        System.out.println("A media é: " + meuFilme.pegaMedia());
+
+    }
+}
---
+++ src/br/com/alura/screenmatch/modelos/Titulo.java
@@ -0,0 +1,56 @@
+package br.com.alura.screenmatch.modelos;
+
+public class Titulo {
+    private String nome;
+    private int anoDeLancamento;
+    private boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    private int duracaoEmMinutos;
+
+    public void exibeFichaTecnica(){
+        System.out.println("Nome: " + this.nome);
+        System.out.println("Ano de lançamento: " + this.anoDeLancamento);
+    }
+
+    public void avalia(double nota){
+        this.somaDasAvaliacoes += nota;
+        this.totalDeAvaliacoes++;
+    }
+
+    public double pegaMedia(){
+        return this.somaDasAvaliacoes / this.totalDeAvaliacoes;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getAnoDeLancamento() {
+        return anoDeLancamento;
+    }
+
+    public void setAnoDeLancamento(int anoDeLancamento) {
+        this.anoDeLancamento = anoDeLancamento;
+    }
+
+    public boolean isIncluidoNoPlano() {
+        return incluidoNoPlano;
+    }
+
+    public void setIncluidoNoPlano(boolean incluidoNoPlano) {
+        this.incluidoNoPlano = incluidoNoPlano;
+    }
+
+    public int getDuracaoEmMinutos() {
+        return duracaoEmMinutos;
+    }
+
+    public void setDuracaoEmMinutos(int duracaoEmMinutos) {
+        this.duracaoEmMinutos = duracaoEmMinutos;
+    }
+}
