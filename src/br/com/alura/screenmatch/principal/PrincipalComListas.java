a4702244b3acfe3b50509778ad3b1c13dfb91d6f
isabel.sleigman <sleimusic@gmail.com>
2023-06-23 17:21:25 -0300

    Trabalhando com listas com objetos

---
+++ src/br/com/alura/screenmatch/principal/PrincipalComListas.java
@@ -0,0 +1,39 @@
+package br.com.alura.screenmatch.principal;
+
+import br.com.alura.screenmatch.modelos.Filme;
+import br.com.alura.screenmatch.modelos.Serie;
+import br.com.alura.screenmatch.modelos.Titulo;
+
+import java.util.ArrayList;
+
+public class PrincipalComListas {
+    public static void main(String[] args) {
+
+        var favorito = new Filme("The Matrix", 1999);
+        favorito.avalia(6);
+        var outro = new Filme("John Wick", 2014);
+        outro.avalia(1);
+        var serie = new Serie("La Casa de Papel", 2017);
+
+        var listaDeAssistidos = new ArrayList<Titulo>();
+        listaDeAssistidos.add(favorito);
+        listaDeAssistidos.add(outro);
+        listaDeAssistidos.add(serie);
+
+        System.out.println("Tamanha da lista: " + listaDeAssistidos.size());
+        System.out.println("Primeiro filme: " + listaDeAssistidos.get(0).getNome());
+
+        listaDeAssistidos.forEach(filmeOuSerie -> {
+            System.out.println(filmeOuSerie.getNome());
+            // Não é o mais correto a se fazer, apenas um exemplo.
+            if(filmeOuSerie instanceof Filme filme && filme.getClassificacao() > 2){
+                System.out.println("Classificação: " + filme.getClassificacao());
+            }
+        });
+
+        //exemplo de outra forma com forEach
+        listaDeAssistidos.forEach(System.out::println);
+
+    }
+
+}
