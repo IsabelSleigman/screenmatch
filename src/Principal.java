07e3e8ba9527d76a0dac22e752a0995ed39ebac5
isabel.sleigman <sleimusic@gmail.com>
2023-06-23 16:33:21 -0300

    Trabalhando com listas

--- src/Principal.java
+++ src/Principal.java
@@ -4,22 +4,24 @@ import br.com.alura.screenmatch.modelos.Episodio;
 import br.com.alura.screenmatch.modelos.Filme;
 import br.com.alura.screenmatch.modelos.Serie;
 
+import java.util.ArrayList;
+
 public class Principal {
     public static void main(String[] args) {
 
-        Filme favorito = new Filme();
+        var favorito = new Filme();
         favorito.setNome("The Matrix");
         favorito.setAnoDeLancamento(1999);
         favorito.setDuracaoEmMinutos(135);
         favorito.setIncluidoNoPlano(true);
 
-        Filme outro = new Filme();
+        var outro = new Filme();
         outro.setNome("John Wick");
         outro.setAnoDeLancamento(2014);
         outro.setDuracaoEmMinutos(101);
         outro.setIncluidoNoPlano(true);
 
-        Serie serie = new Serie();
+        var serie = new Serie();
         serie.setNome("La Casa de Papel");
         serie.setAnoDeLancamento(2017);
         serie.setIncluidoNoPlano(true);
@@ -28,22 +30,32 @@ public class Principal {
         serie.setEpisodiosPorTemporada(10);
         serie.setMinutosPorEpisodio(45);
 
-        CalculadoraDeTempo calculadora = new CalculadoraDeTempo();
+        var calculadora = new CalculadoraDeTempo();
         calculadora.inclui(favorito);
         calculadora.inclui(outro);
         calculadora.inclui(serie);
 
         System.out.println("Tempo total: " + calculadora.getTempoTotal());
 
-        FiltroRecomendacao filtro = new FiltroRecomendacao();
+        var filtro = new FiltroRecomendacao();
         filtro.filtra(favorito);
 
-        Episodio episodio = new Episodio();
+        var episodio = new Episodio();
         episodio.setNumero(1);
         episodio.setSerie(serie);
         episodio.setTotalVisualizacoes(300);
 
         filtro.filtra(episodio);
+
+        ArrayList<Filme> filmes = new ArrayList<Filme>();
+
+        filmes.add(favorito);
+        filmes.add(outro);
+
+        System.out.println("Tamanha da lista: " + filmes.size());
+        System.out.println("Primeiro filme: " + filmes.get(0).getNome());
+
+        filmes.forEach(filme -> System.out.println(filme.getNome()));
 
     }
 }
